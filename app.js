46f81d87c8c569ff988dc5711a37b0eb64b88c09
Calicolive <calicodeep@gmail.com>
2022-11-03 19:34:06 -0500

    added choice array

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 let cells = document.querySelectorAll(".row > div");
+let plays = Array.from(cells);
+let xChoice = [];
+let oChoice = [];
 let player = "X";
 let turn = 0;
+
+let winningCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", cellClicked);
 }
 
-if (turn == "X") {
-  turn = "O";
-} else {
-  turn = "X";
-}
-
 function cellClicked(event) {
   if (event.target.textContent == "") {
     event.target.textContent = player;
-    turn++;
+
     if (player == "X") {
+      xChoice.push(plays.indexOf(event.target));
+      // console.log(xChoice);
+      checkWin();
       player = "O";
     } else {
+      oChoice.push(plays.indexOf(event.target));
+      // console.log(oChoice);
       player = "X";
+    }
+  }
+}
+
+function checkWin() {
+  for (let i = 0; i < winningCombo.length; i++) {
+    if (xChoice.includes(winningCombo[i]));
+    {
+      console.log("test");
     }
   }
 }
